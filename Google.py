492e5e0f0d3d7fc2d64fe92e394a36e7cc6cc4a3
ItaIta123 <66623308+ItaIta123@users.noreply.github.com>
2020-11-05 22:57:03 +0200

    Add files via upload

---
+++ Google.py
@@ -0,0 +1,168 @@
+import requests
+from urllib.parse import urlencode  # a cool tool for making urls
+import googlemaps
+# import prettyprint
+import pprint
+from bs4 import BeautifulSoup
+import keys
+
+api_key = keys.google_api_key()
+
+
+def extract_lat_lng(address_or_postalcode='current', data_type='json', reverse=False, latlng=(0, 0)):
+    """
+    :param address_or_postalcode: address to geocode
+    :param data_type:
+    :param reverse: from geocode to address
+    :param latlng: geocode lat and lng
+    :return: the geolocation: (lat,lng). If the current location (address_or_postalcode) isn't specified I Webscrape
+    the device's current geolocation
+    """
+    # in the reverse case (geocode to address)
+    if reverse is True:
+        lat = latlng[0]
+        lng = latlng[1]
+        url = f'https://maps.googleapis.com/maps/api/geocode/json?latlng={lat},{lng}&key={api_key}'
+        # print(url)
+        r = requests.get(url)
+        address = r.json()['results'][1]['formatted_address']
+        # print(address)
+        return address
+
+    else:
+        # in a case where current_location isn't specified; get the device's geocoding:
+        if address_or_postalcode == 'current':
+            geo_coding_website_html = requests.get('https://tools.keycdn.com/geo')
+            # CHeck for page's availability
+            if geo_coding_website_html.status_code not in range(200, 299):
+                print("Error 404 Page could not be found")
+                return "Error 404 Page could not be found"
+            else:
+                # parse the geocoding
+                src = geo_coding_website_html.content
+                soup = BeautifulSoup(src, 'lxml')
+                # get the specific geocod html value
+                try:
+                    geocode = soup.findAll("dd", class_="col-8 text-monospace")[4].string
+                    # print(geocode) OUTPUT = 32.2696 (lat) / 34.8876 (long)
+                except:
+                    print("Error parsing the geocode from the website. Google.py line 37")
+                else:
+                    # getting only the numbers
+                    geocode = geocode.split()
+                    lat = geocode[0]
+                    lng = geocode[3]
+                    current_location = float(lat), float(lng)
+                    # print(current_location)
+                    # OUTPUT = ('32.2696', '34.8876')
+                    return current_location
+
+        # There is a value for the address_postal code location
+        else:
+            end_point = f'https://maps.googleapis.com/maps/api/geocode/{data_type}'
+            params = {'address': address_or_postalcode, 'key': api_key}
+            url_params = urlencode(params)
+            # print(url_params)
+            url = f'{end_point}?{url_params}'
+            # print(url)
+            r = requests.get(url)
+            # Check for page availability
+            if r.status_code not in range(200, 299):
+                return {}
+            latlng = {}
+            try:
+                # Try to copy the location dict to latlng
+                latlng = r.json()['results'][0]['geometry']['location']
+            except:
+                print("error loading the location from the json data")
+            else:
+                # If the 'try' statement worked latlng = {"lat" : 37.42165019999999, "lng" : -122.0856843 }
+                return latlng.get('lat'), latlng.get('lng')
+
+
+def distance(current_location='current', target_location=None):
+    """
+    :param current_location: can be specified or not. If not: getting the device's current location
+    :param target_location:
+    :return: Time and Seconds
+    """
+    if current_location == 'current':
+        geocode_current_location = extract_lat_lng('current')
+        current_location = extract_lat_lng(reverse=True, latlng=geocode_current_location)
+        # Base url
+        end_point = "https://maps.googleapis.com/maps/api/distancematrix/json?"
+        params = {'origins': current_location, 'destinations': target_location, 'key': api_key}
+        url_params = urlencode(params)
+        final_url = f'{end_point}{url_params}'
+        # print(final_url)
+
+        # get response
+        r = requests.get(final_url)
+        # print(r)
+
+        # return time as text and as seconds
+        # print(r.json())
+        try:
+            time = r.json()['rows'][0]['elements'][0]['duration']['text']
+            seconds = r.json()['rows'][0]['elements'][0]['duration']['value']
+        except:
+            print("Don't be silly, pick something else")
+            return "Don't be silly, pick something else"
+        else:
+            # print/return the total travel time
+            return time
+            # print("Time" , time)
+            # print("seconds", seconds)
+
+    else:
+        # Base url
+        end_point = "https://maps.googleapis.com/maps/api/distancematrix/json?"
+        params = {'origins': current_location, 'destinations': target_location, 'key': api_key}
+        url_params = urlencode(params)
+        final_url = f'{end_point}{url_params}'
+        # print(final_url)
+
+        # get response
+        r = requests.get(final_url)
+        # print(r)
+
+        # return time as text and as seconds
+        # print(r.json())
+        try:
+            time = r.json()['rows'][0]['elements'][0]['duration']['text']
+            seconds = r.json()['rows'][0]['elements'][0]['duration']['value']
+        except:
+            print("Don't be silly, pick something else")
+            return "Don't be silly, pick something else"
+        else:
+            # print/return the total travel time
+            return time
+            # print("Time" , time)
+            # print("seconds", seconds)
+
+
+def places_details(place_name='current', results_type=None):
+    """
+    :param place_name: The place you want to look for POI around
+    :param results_type: https://developers.google.com/places/web-service/supported_types
+    :return: Name of the POI, address , ETA (estimated time arrival)
+    """
+    # Define Client Object - the way to react with Google's maps api:
+    gmaps = googlemaps.Client(key=api_key)  # Takes care the Required key= parameter in the .places_nearby()
+
+    # Define Search:
+    places_result = gmaps.places_nearby(location=extract_lat_lng(place_name), radius=5000, open_now=False,
+                                        type=results_type)
+    # print(places_result['results'])
+    for place in places_result['results']:
+        POI_name = place['name']
+        place_address = place['vicinity']
+        ETA = distance('current', place_address)
+        if POI_name == place_address:
+            pass
+        else:
+            print(f'Name: {POI_name} | Address: {place_address} | ETA: {ETA}')
+
+
+if __name__ == "__main__":
+    places_details("New York", 'coffee')
---
+++ personalAssistant.py
@@ -0,0 +1,216 @@
+import pyttsx3
+import speech_recognition as sr  # Install brew -> install portaudio -> pip3 install PyAudio
+import random
+import time
+import requests
+import wikipedia
+import smtplib
+from Google import places_details, distance
+import keys
+
+
+name = "Karen"
+
+
+# CHANGE RATE
+def speak(data=f"Hey, I am {name}. How can I help you ?"):
+    engine = pyttsx3.init()
+    """VOICE"""
+    # ['Male Robot-0', 'Hebrew-5', 'British man-7', 'Female-10', 'Steven Hokins-11', 'Werid female-17',
+    # 'Japanese/Chinese-18', 'Funny male voice-23', 'Female Russin-27', 'Female - very good - 28', 'Male Indian -32',
+    # 'Female - best 33', 'Australian (?) female - 37']
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice', voices[33].id)
+    engine.say(data)
+    engine.runAndWait()
+
+
+def listen():
+    # obtain audio from the microphone
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("I am listening...")
+        r.adjust_for_ambient_noise(source)
+        audio = r.listen(source)
+        response = ""
+        try:
+            response = r.recognize_google(audio)
+            print("You said: " + response)
+        except sr.UnknownValueError:
+            speak("Sorry I did not get it. Please try again")
+        except sr.RequestError as e:
+            print("Could not request results from Google Speech Recognition service. Check for Internet connection."
+                  " Error message: {0}".format(e))
+
+        return response
+
+
+def voice_assistant(response):
+    if "how are you" in response:
+        speak("I am fine, thank you")
+
+    if "what is your name" == response:
+        speak("My name is Galaxy, the one and only")
+
+    if "what time is it" in response:
+        t = time.localtime()
+        current_time = time.strftime("%H:%M:%S", t)
+        print(current_time)
+        speak(current_time)
+
+    if "how far is the Moon" == response:
+        speak("the moon is 384,400 km away")
+
+    if "how far is the Sun" == response:
+        speak("the Sun is 150 million kilometers away")
+
+    # if "fart" in response:
+    #     speak("Sure, I just need a few seconds to concentrate")
+    #     time.sleep(1.5)
+    #     os.startfile('fart.mp3')
+
+    if "b****" in response:
+        speak("Please speak nicely next time. Beach")
+
+    if "f*****" in response:
+        speak("Please speak nicely next time")
+
+    if "stop the program" in response:
+        speak("Ok,I hope to see you soon!")
+        exit()
+
+    if "close the program" in response:
+        speak("Ok,I hope to see you soon!")
+        exit()
+
+    if "exit the program" in response:
+        speak("Ok,I hope to see you soon!")
+        exit()
+
+    if "tell me a joke" == response:
+        joke_list = [
+            "Never criticize someone until you've walked a mile in their shoes. That way, when you criticize them, "
+            "they won't be able to hear you from that far away. Plus, you'll have their shoes.",
+            "Two fish are in a tank. One says to the other, Do you know how to drive this thing?",
+            "Can a kangaroo jump higher than a house?. Of course, a house doesn't jump at all",
+            "The doctor is saying. I'm sorry but you suffer from a terminal illness and have only 10 to live. "
+            "The patient replies.What do you mean, 10?. 10 what?, 10 Months? 10 Weeks? 10 days? .Then the doctor is "
+            "saying, Nine. Eight. Seven."]
+        speak("Here is one")
+        speak(random.choice(joke_list))
+        speak("Funny, right? ha ha ha ha")
+
+    if "what is the weather" in response:
+        city = response.split("what is the weather in")
+        city = " ".join(city)  # "Gets only the city name"
+        weather(city)
+
+    if "who is" in response:
+        search = response.split('is')
+        search = search[1]
+        search = search.strip()
+        print(search + ":")
+        wiki(search)
+
+    if "who was" in response:
+        search = response.split('was')
+        search = search[1]
+        search = search.strip()
+        print(search + ":")
+        wiki(search)
+
+
+    if "send email" in response:
+        speak("Please enter the receiver email address")
+        receiver_mail_address = input("Receiver email address:")
+        speak("Please record your message:")
+        print("Wait...")
+        message = listen()
+        email(message, receiver_mail_address)
+
+    if "show me places nearby" in response:
+        place = response.split("show me places nearby")
+        place = " ".join(place)  # "Gets only the city name"
+        speak("sure. which kinds of places? Bars? , restaurants? , coffee?")
+        answer = listen()
+        speak(f"Here is a list of the best {answer}  in {place} their address and your estimated time to get there")
+        places_details(place, answer)
+
+    if "how long it will take me get to" in response:
+        place = response.split("how long it will take me get to")
+        place = " ".join(place)  # "Gets only the city name"
+        speak(f"It will take you {distance('current', place)}")
+
+    if "how long will it take me to get to" in response:
+        place = response.split("how long will it take me to get to")
+        place = " ".join(place)  # "Gets only the city name"
+        speak(f"It will take you {distance('current', place)} to get to {place}")
+
+
+def weather(city="Tel Aviv", unit="c"):
+    api_key = keys.weather_api_key()
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={api_key}'
+    # print(url)
+    json_data = requests.get(url).json()
+    # print(json_data)
+    weather_state = (json_data['weather'][0]['main'])
+    tempC = (json_data['main']['temp'])
+    tempF = celsius_to_fahrenheit(json_data['main']['temp'])
+    hum = (json_data['main']['humidity'])
+    if unit == "c":
+        speak(f"The weather in {city} is {weather_state}, the temperature is {tempC} celsius "
+              f"and the humidity is {hum} percent")
+    else:
+        speak(f"The weather is {weather_state}, the temperature is {tempF} fahrenheit and the humidity is {hum}")
+
+
+def wiki(search="Track and Field", sentences=2):
+    try:
+        search_result = wikipedia.summary(search, sentences=sentences)
+    # If she can not find the wiki value
+    except:
+        speak(f"I am sorry, I don't now who is {search} but here is a link that might help:")
+        print(google_search(search))
+    else:
+        url = wikipedia.page(search).url
+        speak(f"{search_result}, for more, click the following link:")
+        print(url)
+
+
+def email(email_content="Hey!", receiver_email='pythontestita123@gmail.com'):
+    """
+    This is the simple email sending syntax:
+    """
+    my_email = 'pythontestita123@gmail.com'
+    email_password = 'python0202'
+    with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+        smtp.ehlo()
+        smtp.starttls()
+        smtp.ehlo()
+
+        smtp.login(my_email, email_password)
+        subject = "This email was sent through my personal assistant!"
+        body = email_content
+
+        msg = f'Subject: {subject}\n\n{body}'
+
+        smtp.sendmail(my_email, receiver_email, msg)
+        speak("Email was successfully sent")
+
+
+def google_search(search):
+    search = search.split(" ")
+    search = '+'.join(search)
+    return f'https://www.google.com/search?sxsrf=ALeKk03EEoudq6N9VEyh7yXFkzzQj4aMDQ%3A1604338782086&ei=XkSgX6LbBKGNlwS_' \
+           f'2JNI&q={search}'
+
+
+def celsius_to_fahrenheit(c):
+    return round((c * 9 / 5) + 32)
+
+
+if __name__ == "__main__":
+    speak()
+    while 1:
+        response = listen()
+        voice_assistant(response)
